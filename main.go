fdc90c2212db63621d0759737c8b65d69cf56a26
George Pugachev <g.v.pugachev@gmail.com>
2019-01-21 20:22:42 +0200

    ok result

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import "golang.org/x/tour/reader"
 type MyReader struct{}
 
 func (r MyReader) Read(b []byte) (int, error) {
-	b[0] = 'A'
-	return 1, nil
+	for i := range b {
+		b[i] = 'A'
+	}
+
+	return len(b), nil
 }
 
 func main() {
